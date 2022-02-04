baa319ac7707950714622c7ff420d199e71b1a75
AntoBo <anton.bondarenkooooo@gmail.com>
2022-02-04 12:05:28 +0200

    styling mobile slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,17 +1,36 @@
+const prevArrow = $('.reviews__switch--left');
+const nextArrow = $('.reviews__switch--right');
+
 $('.slider-for').slick({
+  dots: false,
   slidesToShow: 1,
   slidesToScroll: 1,
   arrows: false,
-  // fade: true,
   asNavFor: '.slider-nav',
-  autoplay: true,
-  autoplaySpeed: 3000,
+  // autoplay: true,
+  // autoplaySpeed: 2000,
+  mobileFirst: true,
 });
 $('.slider-nav').slick({
+  mobileFirst: true,
   slidesToShow: 3,
   slidesToScroll: 1,
   asNavFor: '.slider-for',
   dots: false,
+  variableWidth: true,
   centerMode: true,
-  focusOnSelect: true,
+  // prevArrow: prevArrow,
+  // nextArrow: nextArrow,
+  // focusOnSelect: true,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 7,
+        slidesToScroll: 1,
+        infinite: false,
+        // centerMode: false,
+      },
+    },
+  ],
 });
