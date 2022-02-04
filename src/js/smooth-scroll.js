843b3a2145a84d74a0db87ab876223f935b35335
AnthonyUPTN <tagirovantn@gmail.com>
2022-02-04 10:35:51 +0200

    1й

--- src/js/smooth-scroll.js
+++ src/js/smooth-scroll.js
@@ -1,19 +1,24 @@
 // Найти все ссылки начинающиеся на #
-const anchors = document.querySelectorAll('[href^="#"]');
+// const anchors = document.querySelectorAll('a[href^="#"]');
+const anchorsDt = document.querySelectorAll('.header__nav-link');
+const anchorsMob = document.querySelectorAll('.menu-container__link');
 const body = document.querySelector('.body');
 const mobileMenuRef = document.querySelector('[data-menu]');
 const menuBtnRef = document.querySelector('[data-menu-button]');
-const a = document.querySelector(".header__logo");
-const span = document.querySelector(".header__logo-title");
+const a = document.querySelector('.header__logo');
+const span = document.querySelector('.header__logo-title');
 
-// Цикл по всем ссылкам
-for (let anchor of anchors) {
+// console.log(anchorsDt);
+// console.log(anchorsMob);
+
+// Цикл по всем ссылкам десктопа
+for (let anchor of anchorsDt) {
   anchor.addEventListener('click', function (e) {
-    body.classList.toggle('no-scroll');
+    // body.classList.toggle('no-scroll');
     mobileMenuRef.classList.toggle('is-open');
     menuBtnRef.classList.toggle('is-active');
-    a.classList.toggle("white");
-    span.classList.toggle("text-color");
+    a.classList.toggle('white');
+    span.classList.toggle('text-color');
 
     e.preventDefault(); // Предотвратить стандартное поведение ссылок
     // Атрибут href у ссылки, если его нет то перейти к body (наверх не плавно)
@@ -26,3 +31,22 @@ for (let anchor of anchors) {
   });
 }
 
+// Цикл по всем ссылкам моб и таб
+for (let anchor of anchorsMob) {
+  anchor.addEventListener('click', function (e) {
+    body.classList.toggle('no-scroll');
+    mobileMenuRef.classList.toggle('is-open');
+    menuBtnRef.classList.toggle('is-active');
+    a.classList.toggle('white');
+    span.classList.toggle('text-color');
+
+    e.preventDefault(); // Предотвратить стандартное поведение ссылок
+    // Атрибут href у ссылки, если его нет то перейти к body (наверх не плавно)
+    const goto = anchor.hasAttribute('href') ? anchor.getAttribute('href') : 'body';
+    // Плавная прокрутка до элемента с id = href у ссылки
+    document.querySelector(goto).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  });
+}
