a0ba39b03834c62c24deb18e513d7037f381436f
yaroslav.kosytsia <jaroslavkositsa@gmail.com>
2022-02-04 13:55:16 +0200

    finish slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,16 +1,14 @@
-const prevArrow = $('.reviews__switch--left');
-const nextArrow = $('.reviews__switch--right');
-
 $('.slider-for').slick({
   dots: false,
   slidesToShow: 1,
   slidesToScroll: 1,
   arrows: false,
   asNavFor: '.slider-nav',
-  // autoplay: true,
-  // autoplaySpeed: 2000,
+  autoplay: true,
+  autoplaySpeed: 2000,
   mobileFirst: true,
 });
+
 $('.slider-nav').slick({
   mobileFirst: true,
   slidesToShow: 3,
@@ -19,18 +17,4 @@ $('.slider-nav').slick({
   dots: false,
   variableWidth: true,
   centerMode: true,
-  // prevArrow: prevArrow,
-  // nextArrow: nextArrow,
-  // focusOnSelect: true,
-  responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 7,
-        slidesToScroll: 1,
-        infinite: false,
-        // centerMode: false,
-      },
-    },
-  ],
 });
