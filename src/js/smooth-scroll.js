47d13ea6c236fdb4f4e281327a61ee35e940b38b
AntoBo <anton.bondarenkooooo@gmail.com>
2022-02-04 10:47:01 +0200

    fix

--- src/js/smooth-scroll.js
+++ src/js/smooth-scroll.js
@@ -1,5 +1,3 @@
-// Найти все ссылки начинающиеся на #
-// const anchors = document.querySelectorAll('a[href^="#"]');
 const anchorsDt = document.querySelectorAll('.header__nav-link');
 const anchorsMob = document.querySelectorAll('.menu-container__link');
 const body = document.querySelector('.body');
@@ -8,18 +6,9 @@ const menuBtnRef = document.querySelector('[data-menu-button]');
 const a = document.querySelector('.header__logo');
 const span = document.querySelector('.header__logo-title');
 
-// console.log(anchorsDt);
-// console.log(anchorsMob);
-
 // Цикл по всем ссылкам десктопа
 for (let anchor of anchorsDt) {
   anchor.addEventListener('click', function (e) {
-    // body.classList.toggle('no-scroll');
-    mobileMenuRef.classList.toggle('is-open');
-    menuBtnRef.classList.toggle('is-active');
-    a.classList.toggle('white');
-    span.classList.toggle('text-color');
-
     e.preventDefault(); // Предотвратить стандартное поведение ссылок
     // Атрибут href у ссылки, если его нет то перейти к body (наверх не плавно)
     const goto = anchor.hasAttribute('href') ? anchor.getAttribute('href') : 'body';
